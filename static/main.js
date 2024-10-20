2fa81eed6b1958026cd7bfb09432f33fe00fc2cb
Aaron Kofsky <aaronko@umich.edu>
2024-10-19 23:09:15 -0400

    Improve generation time estimation

--- static/main.js
+++ static/main.js
@@ -1,3 +1,9 @@
+let IS_PLAYING = false;
+let ALREADY_FETCHING = false;
+
+/** @type {{ elapsed: number, stepSeconds: number }[]} */
+let RECENT_STEPSECONDS = [];
+let RECENT_STEPSECONDS_INDEX = 0;
 /**
  * Constructor type, such as "HTMLElement" 
  * @template T
@@ -44,21 +50,29 @@ function getDestinationNode() {
 }
 
 /**
+ * @param {number} steps
+ * @param {number} length
  * @returns {number} The estimated amount of time, in seconds, that it takes to generate the next batch of audio.
  */
-function estimatedGenerationTime() {
-    const steps = parseInt(steps_input.value);
-    const length = parseFloat(length_input.value);
-    // at 60 seconds, about 2 iterations per second
-    // at 50 seconds, about 3 iterations per second
-    // at 40 seconds, about 4 iterations per second
-    // at 30 seconds, about 5 iterations per second
-    // at 20 seconds, about 7 iterations per second
-    // at 10 seconds, about 10 iterations per second
-    // see graph: https://www.desmos.com/calculator/hai0oucjod
-    let stepsPerSecond = 10.0 - length / 6.0;
-    stepsPerSecond = Math.min(Math.max(stepsPerSecond, 0.5), 10.0);
-    return (steps / stepsPerSecond);
+function estimatedGenerationTime(steps, length) {
+    if (RECENT_STEPSECONDS.length == 0) {
+        // at 60 seconds, about 2 iterations per second
+        // at 50 seconds, about 3 iterations per second
+        // at 40 seconds, about 4 iterations per second
+        // at 30 seconds, about 5 iterations per second
+        // at 20 seconds, about 7 iterations per second
+        // at 10 seconds, about 10 iterations per second
+        // see graph: https://www.desmos.com/calculator/hai0oucjod
+        let stepsPerSecond = 10.0 - length / 6.0;
+        stepsPerSecond = Math.min(Math.max(stepsPerSecond, 0.5), 10.0);
+        return (steps / stepsPerSecond);
+    }
+    // "data" seconds generated per second
+    const total_step_seconds = RECENT_STEPSECONDS.map(x => x.stepSeconds).reduce((a, b) => a + b, 0.0)
+    const total_elapsed = RECENT_STEPSECONDS.map(x => x.elapsed).reduce((a, b) => a + b, 0.0)
+    const seconds_steps_per_second = total_step_seconds / total_elapsed;
+    const estimate = length * steps / seconds_steps_per_second;
+    return estimate;
 }
 
 /**
@@ -70,16 +84,24 @@ async function queueIfNeeded() {
     }
 
 
-    const enoughBuffer = remainingBufferTime() > estimatedGenerationTime() * 1.1 ;
-    if (!enoughBuffer && !ALREADY_FETCHING) {
-        console.log("Requesting at remaining buffer: ", remainingBufferTime());
-        ALREADY_FETCHING = true;
-        const promptUrl = getPromptUrl();
-        const sourceNode = await getAudioBufferSourceNode(promptUrl);
-        queueAudio(sourceNode);
-        seed_input.value = (parseInt(seed_input.value) + 1).toString();
-        ALREADY_FETCHING = false;
+    const settings = getPromptSettings();
+    if (settings != null) {
+        const minimumBuffer = estimatedGenerationTime(settings.steps, settings.length) * 1.25 + 2.0;
+        const enoughBuffer = remainingBufferTime() > minimumBuffer;
+        if (!enoughBuffer && !ALREADY_FETCHING) {
+            ALREADY_FETCHING = true;
+            const promptUrl = getRadioUrl(settings);
+            const { node, elapsed } = await getAudioBufferSourceNode(promptUrl);
+            console.log(`Took ${elapsed} seconds to generate audio`);
+            queueAudio(node);
+            recordGenerationStats(settings, elapsed);
+            setEstimatedTimeDisplay()
+    
+            seed_input.value = (parseInt(seed_input.value) + 1).toString();
+            ALREADY_FETCHING = false;
+        }
     }
+
 }
 
 /**
@@ -92,24 +114,66 @@ function setVolumeFromSlider() {
 }
 
 /**
+ * @typedef {{ 
+ *     positive_prompt: string | null;
+ *     negative_prompt: string | null;
+ *     cfg: number;
+ *     sigma_min: number;
+ *     sigma_max: number;
+ *     seed: number;
+ *     steps: number;
+ *     length: number;
+ *     debug_save: boolean; 
+ * }} PromptSettings 
+ * @returns {PromptSettings | null}
+ */
+function getPromptSettings() {
+    let negative_prompt = null;
+    if (negative_prompt_enable_checkbox.checked || negative_prompt_textarea.value != "") {
+        negative_prompt = negative_prompt_textarea.value;
+    }
+
+    if (!cfg_input.checkValidity() || 
+        !sigma_min_input.checkValidity() ||
+        !sigma_max_input.checkValidity() ||
+        !seed_input.checkValidity() ||
+        !steps_input.checkValidity() ||
+        !length_input.checkValidity()) {
+        return null;
+    }
+    return {
+        "positive_prompt": positive_prompt_textarea.value,
+        "negative_prompt": negative_prompt,
+        "cfg": parseFloat(cfg_input.value),
+        "sigma_min": parseFloat(sigma_min_input.value),
+        "sigma_max": parseFloat(sigma_max_input.value),
+        "seed": parseInt(seed_input.value),
+        "steps": parseInt(steps_input.value),
+        "length": parseFloat(length_input.value),
+        "debug_save": save_to_disk_checkbox.checked,
+    }
+}
+
+/**
  * Build the radio URL along with it's parameters using the controls on the page.
+ * @param {PromptSettings} settings 
  * @returns {URL} The prompt URL
  */
-function getPromptUrl() {
+function getRadioUrl(settings) {
     const radio_url = new URL("/radio", window.location.toString());
-    
-    radio_url.searchParams.set("positive_prompt", positive_prompt_textarea.value);
-    const negative_prompt = negative_prompt_textarea.value;
-    if (negative_prompt_enable_checkbox.checked || negative_prompt != "") {
-        radio_url.searchParams.set("negative_prompt", negative_prompt);
+    if (settings.positive_prompt != null) {
+        radio_url.searchParams.set("positive_prompt", settings.positive_prompt);
     }
-    radio_url.searchParams.set("cfg", cfg_input.value);
-    radio_url.searchParams.set("sigma_min", sigma_min_input.value);
-    radio_url.searchParams.set("sigma_max", sigma_max_input.value);
+    if (settings.negative_prompt != null) {
+        radio_url.searchParams.set("negative_prompt", settings.negative_prompt);
+    }
+    radio_url.searchParams.set("cfg_scale", settings.cfg.toString());
+    radio_url.searchParams.set("sigma_min", settings.sigma_min.toString());
+    radio_url.searchParams.set("sigma_max", settings.sigma_max.toString());
 
-    radio_url.searchParams.set("seed", seed_input.value);
-    radio_url.searchParams.set("steps", steps_input.value);
-    radio_url.searchParams.set("length", length_input.value);
+    radio_url.searchParams.set("seed", settings.seed.toString());
+    radio_url.searchParams.set("steps", settings.steps.toString());
+    radio_url.searchParams.set("length", settings.length.toString());
 
     if (save_to_disk_checkbox.checked) {
         radio_url.searchParams.set("debug_save", "true");
@@ -152,7 +216,6 @@ function queueAudio(audioBufferSourceNode) {
     const buffer = assertNotNull(audioBufferSourceNode.buffer);
     const duration = buffer.duration;
     const queueTime = getLatestQueuedOrNow();
-    console.log(`Queuing audio at ${queueTime}. Remaining buffer time: ${remainingBufferTime()}`);
     audioBufferSourceNode.start(queueTime);
     LATEST_QUEUED_TIME = queueTime + duration;
 }
@@ -160,24 +223,26 @@ function queueAudio(audioBufferSourceNode) {
 /**
  * Create an AudioBufferSourceNode from the given URL.
  * @param {URL} url The radio URL endpoint. This is what is fetched from to download the audio. 
- * @returns {Promise<AudioBufferSourceNode>} An AudioBufferSourceNode containing the generated audio
+ * @returns {Promise<{node: AudioBufferSourceNode, elapsed: number}>} An AudioBufferSourceNode containing the generated audio
  */
 async function getAudioBufferSourceNode(url) {
+    const now = Date.now();
     const arrayBuffer = await fetch(url).then((res) => res.arrayBuffer());
     const audioBuffer = await audioCtx.decodeAudioData(arrayBuffer);
-    const source = audioCtx.createBufferSource();
-    source.buffer = audioBuffer
+    const node = audioCtx.createBufferSource();
+    node.buffer = audioBuffer
 
-    source.connect(getDestinationNode());
-    return source;
+    node.connect(getDestinationNode());
+    const elapsed = Date.now() - now;
+    return {
+        node,
+        elapsed: elapsed / 1000.0,
+    };
 }
 
 const audioCtx = new window.AudioContext();
 const gainNode = audioCtx.createGain();
 gainNode.connect(audioCtx.destination);
-
-let IS_PLAYING = false;
-let ALREADY_FETCHING = false;
 
 
 const positive_prompt_textarea = getElementTyped("positive_prompt", HTMLTextAreaElement);
@@ -229,10 +294,30 @@ setRemainingBuffer();
 setInterval(queueIfNeeded, 1000);
 
 function setEstimatedTimeDisplay() {
-    estimated_time_display.innerText = estimatedGenerationTime().toFixed(1)
+    const settings = getPromptSettings();
+    if (settings != null) {
+        estimated_time_display.innerText = estimatedGenerationTime(settings.steps, settings.length).toFixed(1)
+    }
 }
 
 setInterval(setRemainingBuffer, 100);
 function setRemainingBuffer() {
     remaining_buffer_display.innerText = remainingBufferTime().toFixed(1);
+}
+
+/**
+ * @param {PromptSettings} settings 
+ * @param {number} elapsed 
+ */
+function recordGenerationStats(settings, elapsed) {
+    const item = {
+        stepSeconds: settings.steps * settings.length,
+        elapsed
+    };
+    if (RECENT_STEPSECONDS.length < 10) {
+        RECENT_STEPSECONDS.push(item)
+    } else {
+        RECENT_STEPSECONDS[RECENT_STEPSECONDS_INDEX] = item;
+        RECENT_STEPSECONDS_INDEX = (RECENT_STEPSECONDS_INDEX + 1) % 10;
+    }
 }
